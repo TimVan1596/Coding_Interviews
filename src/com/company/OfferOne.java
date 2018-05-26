4c37224e8230e74cc5d8cc182a8bd0cf5f6778e7
TimVan <877020296@qq.com>
2018-05-26 22:45:13 +0800

    第一题

--- src/com/company/OfferOne.java
+++ src/com/company/OfferOne.java
@@ -2,20 +2,34 @@ package com.company;
 
 //在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
-public class Solution {
-    public boolean Find(int target, int [][] array) {
-
-
-    }
-}
 
 /**
  * @author 87702
 * */
 public class OfferOne {
 
+    final private static int LEN = 5;
+
     public static void main(String[] args) {
-        int[][] array = new int[5][5];
+
+
+        int[][] array = new int[LEN][LEN];
+
+        for (int i = 0; i < LEN ; ++i) {
+            for (int j = 0; j < LEN; ++j) {
+                array[i][j] = i*10+j;
+            }
+        }
+
+        Solution solution = new Solution();
+        boolean result  = solution.Find(LEN,array);
+
+        if (result){
+
+        }
+        else{
+
+        }
 
 
     }
---
+++ src/com/company/Solution.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * @author TimVan
+ * @date 2018/5/26 22:43
+ */
+class Solution {
+    public boolean Find(int target, int [][] array) {
+
+        for (int i = 0; i < array.length; ++i) {
+            for (int j = 0; j < array[i].length; ++j) {
+                System.out.println(array[i][j]);
+            }
+        }
+
+        return false;
+    }
+}
+
